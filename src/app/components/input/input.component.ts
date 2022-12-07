5e29e4b7f8e8670e40ff09b76f346a7b16409593
Yevgeniy Kolesnik <107919667+Yevgeniy-Kolesnik@users.noreply.github.com>
2022-12-07 15:03:49 +0100

    First commit

---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.css'],
+})
+export class InputComponent implements OnInit {
+  easyColor: string = 'grey';
+  mediumColor: string = 'grey';
+  strongColor: string = 'grey';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onKey(event: any) {
+    const inputValue = event.target.value;
+    const inputLength = inputValue.length;
+
+    const easyPassword = /^[a-z]+$|^\d+$|^[@$!%*#?&]+$/i;
+    const mediumPassword = /^[a-z\d]+$|^[\d@$!%*#?&]+$|^[a-z@$!%*#?&]+$/i;
+    const strongPassword = /^[A-Za-z\d@$!%*#?&]+$/i;
+
+    if (inputLength === 0) {
+      this.easyColor = 'grey';
+      this.mediumColor = 'grey';
+      this.strongColor = 'grey';
+    } else if (inputLength < 8) {
+      this.easyColor = 'red';
+      this.mediumColor = 'red';
+      this.strongColor = 'red';
+    } else if (inputValue.match(easyPassword)) {
+      this.easyColor = 'red';
+      this.mediumColor = 'grey';
+      this.strongColor = 'grey';
+    } else if (inputValue.match(mediumPassword)) {
+      this.easyColor = 'yellow';
+      this.mediumColor = 'yellow';
+      this.strongColor = 'grey';
+    } else if (inputValue.match(strongPassword)) {
+      this.easyColor = 'lime';
+      this.mediumColor = 'lime';
+      this.strongColor = 'lime';
+    }
+  }
+}
